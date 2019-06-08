df62935eb9235a5d00d6671a3401c007340d1287
StefanKeres1995 <2180203@my.ipleiria.pt>
2019-06-08 17:18:37 +0100

    Jenkins changed the ip again

--- src/test/java/StepsDefUS1.java
+++ src/test/java/StepsDefUS1.java
@@ -76,9 +76,11 @@ public class StepsDefUS1 {
     }*/
     @Before
     public void setUp() {
+        //maybe the slashes
         System.setProperty("webdriver.chrome.driver","/drivers/chromedriver.exe");
         //System.setProperty("phantomjs.binary.path", "drivers\\phantomjs.exe");
         ChromeOptions options = new ChromeOptions();
+        //try binary later
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
         try {
