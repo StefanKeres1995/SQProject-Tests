3aa0bfd7da3b0d96407c6fd4014b59546bd36670
lnace <35407136+2150689@users.noreply.github.com>
2019-06-08 11:25:13 +0100

    "java.lang.NoClassDefFoundError: Could not initialize class StepsDefUS1"

--- src/test/java/StepsDefUS1.java
+++ src/test/java/StepsDefUS1.java
@@ -8,9 +8,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.BufferedReader;
