40276a751cf62cd6520d0049b6ca2e39e2fffaef
lnace <35407136+2150689@users.noreply.github.com>
2019-06-08 14:09:35 +0100

    Re-filled the /var/lib/jenkins/tools/chromedriver/chromeDriver.

--- src/test/java/StepsDefUS1.java
+++ src/test/java/StepsDefUS1.java
@@ -33,7 +33,7 @@ public class StepsDefUS1 {
     private static final String NULL_STRING = "--------------";
     static {
         Logger.getLogger("").setLevel(Level.OFF);
-        System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/tools/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/tools/chromedriver/chromedriver");
         System.setProperty("phantomjs.binary.path", "drivers/phantomjs.exe");
         System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
         if (driver == null) {
