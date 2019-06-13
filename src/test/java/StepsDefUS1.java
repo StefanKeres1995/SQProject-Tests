b02b14480eaaee7eed090d422e60e5384423a3fc
lnace <35407136+2150689@users.noreply.github.com>
2019-06-13 19:24:00 +0100

    cleared code

--- src/test/java/StepsDefUS1.java
+++ src/test/java/StepsDefUS1.java
@@ -13,9 +13,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -26,9 +24,6 @@ import java.net.URL;
 import java.util.List;
 
 import static junit.framework.TestCase.assertEquals;
-
-//import org.openqa.selenium.phantomjs.PhantomJSDriver;
-//import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 
 public class StepsDefUS1 {
     private static WebDriver driver;
@@ -44,48 +39,9 @@ public class StepsDefUS1 {
     {
         if(driver == null) {
             //System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
-            //System.setProperty("phantomjs.binary.path", "drivers/phantomjs.exe");
-            //System.setProperty("webdriver.gecko.driver", "drivers/geckodriverx.exe");
-            //String path = "/home/glnaceg/firefox/geckodriverx";
-            //System.setProperty("webdriver.gecko.driver", "/home/glnaceg/drivers/geckodriverx");
-
-
             ChromeOptions options = new ChromeOptions();
-            //options.setBinary("/home/glnaceg/chromedrivers/chromedriver76");
-            //options.addArguments("--no-sandbox"); // Bypass OS security model, MUST BE THE VERY FIRST OPTION
             options.addArguments("--headless");
-            /*options.addArguments("disable-infobars"); // disabling infobars
-            options.addArguments("--disable-extensions"); // disabling extensions
-            options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-            //options.addArguments("--marionette-port");
-            //options.addArguments("2828");
-            options.addArguments("--ignore-certificate-errors");
-            options.addArguments("--disable-popup-blocking");
-            options.addArguments("--incognito");*/
-
             driver = new ChromeDriver(options);
-
-
-
-            //driver = new ChromeDriver();
-            //driver = new HtmlUnitDriver();
-
-            //webClient = new WebClient();
-
-            //driver = new PhantomJSDriver();
-
-            //driver = new FirefoxDriver(getDefaultFirefoxOptions());
-
-            //System.setProperty("webdriver.gecko.driver", "/home/glnaceg/firefox/geckodriverx");
-            //DesiredCapabilities dc = new DesiredCapabilities();
-            //dc.setCapability("marionatte", false);
-            /*FirefoxOptions opt = new FirefoxOptions();
-            opt.setBinary("/home/glnaceg/firefox/geckodriverx");
-            opt.setLogLevel(FirefoxDriverLogLevel.ERROR);
-            opt.setHeadless(true);
-            //opt.merge(dc);
-            FirefoxDriver driver =  new FirefoxDriver(opt);*/
-
             try {
                 getHTML("http://contactsqs2.apphb.com/Service.svc/rest/contacts");
             } catch (Exception e) {
@@ -96,33 +52,14 @@ public class StepsDefUS1 {
 
     @After
     public void tearDown() throws Exception {
-        //System.clearProperty("webdriver.gecko.driver");
+        driver.close();
         //driver.close();
-        //We won't close up here. These two commands shall be used in Jenkins, after the automaticCode runs.
-        //ps -ef | grep firefox | awk '{print $2}' | xargs kill -9
-        //ps -ef | grep geckodriverx | awk '{print $2}' | xargs kill -9
     }
 
     @Given("^I access the landing page of COS$")
     public void iAccessTheLandingPageOfCOS() throws Throwable {
         driver.get("http://35.246.92.202/");
-
-        /*htmlPage = webClient.getPage("http://35.246.92.202/");
-        webClient.setAjaxController(new NicelyResynchronizingAjaxController());
-        webClient.waitForBackgroundJavaScript(30 * 1000);
-        webClient.waitForBackgroundJavaScriptStartingBefore(30 * 1000);
-        webClient.getOptions().setJavaScriptEnabled(true);
-        for (int i = 0; i < 20; i++){
-            List<?> strings = htmlPage.getByXPath(".//table[@id='contactsTable']/tbody/tr");
-            if(strings.size() != 0){
-                break;
-            }
-            synchronized (htmlPage){
-                htmlPage.wait(500);
-            }
-        }*/
         assertEquals ("Contacts Landing Page",driver.getTitle());
-        //assertEquals ("Contacts Landing Page", htmlPage.getTitleText());
     }
 
     @Then("^the title of the page should be \"([^\"]*)\"$")
@@ -173,18 +110,7 @@ public class StepsDefUS1 {
                 System.out.println("Something ");
             }
         }while(!asd);
-        //wait.until(ExpectedConditions.numberOfElementsToBe(By.xpath(".//table[@id='contactsTable']/tbody/tr"), 10));
-        //wait.until(ExpectedConditions.);
-
-        //Click on the paginator
-        //driver.findElements(By.xpath(".//div[@id='contactsTable_paginate']/span/a[3]")).get(0).click();
-
-        //Get value
-        //Select select = new Select(driver.findElements(By.xpath(".//select[@name='contactsTable_length']")).get(0));
-        //int pagination = Integer.parseInt(select.getFirstSelectedOption().getText());
-
         List<WebElement> strings = driver.findElements(By.xpath(".//table[@id='contactsTable']/tbody/tr[2]/td"));
-        //List<?> strings = htmlPage.getByXPath(".//table[@id='contactsTable']/tbody/tr[2]/td");
         checkIntegrityOfContact(Integer.parseInt(strings.get(0).getText()) - 1, strings);
     }
 
@@ -220,17 +146,5 @@ public class StepsDefUS1 {
         }else{
             assertEquals(contacts[id].getCity(), strings.get(5).getText());
         }
-    }
-
-    static FirefoxOptions getDefaultFirefoxOptions() {
-        return new FirefoxOptions()
-                .setLegacy(false)
-                .setBinary("/home/glnaceg/firefox41/firefox/firefox")
-                .setHeadless(true)
-                .addArguments("--marionette-port")
-                .addArguments("2828");
-                //.addArguments("--headless");
-                //.addPreference("devtools.selfxss.count", 100) //this helps to use console and evaluate xpath, e.g. $x(".//xpath")
-                //.setLogLevel(TRACE);
     }
 }
