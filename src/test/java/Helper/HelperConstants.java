13a5476540dd7df83bb3636448289a4dea7a81a8
lnace <35407136+2150689@users.noreply.github.com>
2019-06-16 19:31:20 +0100

    Changed the IP - Possibilitiy to see everything working

--- src/test/java/Helper/HelperConstants.java
+++ src/test/java/Helper/HelperConstants.java
@@ -4,5 +4,5 @@ public class HelperConstants {
     public static final int WaitCondition_TitleContains = 0;
     public static final int WaitCondition_ElementToBeClickable = 1;
     public static final String NULL_STRING = "--------------";
-    public static final String IP_ADDRESS_INDEX = "http://35.246.92.202/";
+    public static final String IP_ADDRESS_INDEX = "http://35.246.50.214/";
 }
